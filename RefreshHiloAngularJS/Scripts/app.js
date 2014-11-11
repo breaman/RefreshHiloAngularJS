20f8a5a371d335a331763b16b9182a61602df0f3
Michael Stokesbary <mike@stokesbary.me>
2014-11-10 18:41:19 -0800

    Refactored out the hard coded javascript in the controller code and exported it to a WebAPI method. Then used the $http service to retrieve it

--- RefreshHiloAngularJS/Scripts/app.js
+++ RefreshHiloAngularJS/Scripts/app.js
@@ -20,17 +20,17 @@
         .otherwise('/');
     }
 
-    function MyController() {
+    function MyController($http) {
         var vm = this;
         vm.sortBy = 'name';
         vm.reverse = false;
         vm.currentPerson = {};
-        vm.people = [
-            { name: 'Inigo Montoya', gifts: [{ name: 'My Father', description: 'I just want my father back', price: 'priceless', enteredOn: '2014-11-05' }, { name: 'Six Fingered Man', description: 'He killed my father, he should prepare to die', price: '5000', enteredOn: '2014-11-06' }] },
-            { name: 'Princess Buttercup', gifts: [{ name: 'Rodent of Unusual Size', description: 'These are really cool', price: '300', enteredOn: '2014-10-30' }] },
-            { name: 'Fezzik', gifts: [{ name: 'Dr. Seuss Books', description: 'Best rhyming books around. Get these please.', price: '20', enteredOn: '2013-11-05' }, { name: 'Big rocks', description: 'They are a lot of fun to throw', price: '2', enteredOn: '2014-09-05' }] },
-            { name: 'Westley', gifts: [{ name: '50 years of memories', description: 'I lost these somewhere along the way, would love to get them back', price: '50000', enteredOn: 'N/A' }, { name: 'Miracle Pill', description: 'It tastes good in chocolate', price: '65', enteredOn: '2014-08-06' }, { name: 'Brute Squad', description: 'Need it so we can storm the castle', price: '200', enteredOn: '2014-08-07' }] }
-        ];
+
+        $http.get('/data/people').success(function (data) {
+            vm.people = data;
+        }).error(function () {
+            alert('Something went horribly wrong.');
+        });
 
         vm.setCurrentUser = function (person) {
             vm.currentPerson = person;
---
+++ RefreshHiloAngularJS/data/PeopleController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace RefreshHiloAngularJS.api
+{
+    public class PeopleController : ApiController
+    {
+        public Person[] Get()
+        {
+            return new List<Person>()
+            {
+                new Person {Name = "Inigo Montoya",
+                    Gifts = new List<Gift> {
+                        new Gift{Name="My Father", Description = "I just want my father back", Price = Decimal.MaxValue, EnteredOn = new DateTime(2014, 11, 5)},
+                        new Gift{Name="Six Fingered Man", Description = "He killed my father, he should prepare to die", Price = 5000, EnteredOn = new DateTime(2014, 11, 6)}
+                }},
+                new Person {Name = "Princess Buttercup",
+                    Gifts = new List<Gift> {
+                        new Gift{Name="Rodent of Unusual Size", Description = "These are really cool", Price = 300, EnteredOn = new DateTime(2014, 10, 30)}
+                }},
+                new Person {Name = "Fezzik",
+                    Gifts = new List<Gift> {
+                        new Gift{Name="Dr. Seuss Books", Description = "Best rhyming books around. Get these please.", Price = 20, EnteredOn = new DateTime(2013, 11, 5)},
+                        new Gift{Name="Big rocks", Description = "They are a lot of fun to throw", Price = 2, EnteredOn = new DateTime(2014, 9, 5)}
+                }},
+                new Person {Name = "Westley",
+                    Gifts = new List<Gift> {
+                        new Gift{Name="50 years of memories", Description = "I lost these somewhere along the way, would love to get them back", Price = 50000, EnteredOn = DateTime.MinValue},
+                        new Gift{Name="Miracle Pill", Description = "It tastes good in chocolate", Price = 65, EnteredOn = new DateTime(2014, 8, 6)},
+                        new Gift{Name="Brute Squad", Description = "Need it so we can storm the castle", Price = 200, EnteredOn = new DateTime(2014, 8, 7)}
+                }},
+            }.ToArray();
+        }
+    }
+
+    public class Person
+    {
+        public string Name { get; set; }
+        public List<Gift> Gifts { get; set; }
+    }
+
+    public class Gift
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public DateTime EnteredOn { get; set; }
+    }
+}
